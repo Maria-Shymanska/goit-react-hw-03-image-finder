0beed7fad46acd6239fb28b8d2d0f20f58e39c09
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-12 20:49:19 +0200

    Fixed App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,23 +12,19 @@ export class App extends Component {
   state = {
     query: '',
     page: 1,
-    imagesOnPage: 0,
-    totalImage: 0,
+    totalHits: 0,
     isLoading: false,
     showModal: false,
-    images: null,
+    images: [],
     error: null,
     currentImageUrl: null,
     currentImageDescription: null,
   };
-
   componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    // console.log(this.state)
-    if (prevState.query !== query) {
+    const { query, page, images } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
       this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      fetchImages(query)
+      fetchImages(query, page)
         .then(({ hits, totalHits }) => {
           if (totalHits === 0) {
             toast.warn(
@@ -36,11 +32,14 @@ export class App extends Component {
             );
             return;
           }
-
-          if (totalHits > 0) {
+          if (page === 1) {
             toast.info(`Hooray! We found ${totalHits} images.`);
           }
-
+          if (page * 12 >= totalHits) {
+            toast.info(
+              `We're sorry, but you've reached the end of search results.`
+            );
+          }
           const gallery = hits.map(
             ({ id, tags, webformatURL, largeImageURL }) => ({
               id,
@@ -50,70 +49,33 @@ export class App extends Component {
             })
           );
 
-          return this.setState({
-            page: 1,
-            images: gallery,
-            imagesOnPage: gallery.length,
-            totalImage: totalHits,
-          });
-        })
-        .catch(error => this.setState({ error }))
-        .finally(() =>
-          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
-        );
-    }
-
-    if (prevState.page !== page && page !== 1) {
-      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
-
-      fetchImages(query, page)
-        .then(({ hits, totalHits }) => {
-          if (page * 12 >= totalHits) {
-            toast.info(
-              `We're sorry, but you've reached the end of search results.`
-            );
-          }
-
-          console.log(hits);
-          const gallery = hits.map(
-            ({ id, tags, webformatURL, largeImageURL }) => ({
-              id: id,
-              description: tags,
-              smallImage: webformatURL,
-              largeImage: largeImageURL,
-            })
-          );
-
-          return this.setState(({ images, imagesOnPage }) => {
+          this.setState(() => {
             return {
               images: [...images, ...gallery],
-              imagesOnPage: imagesOnPage + gallery.length,
+              showBtn: page * 12 < totalHits,
             };
           });
         })
-        .catch(error => this.setState({ error }))
+        .catch(error => toast.error(error.message, 'Something went wrong!'))
         .finally(() =>
           this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
         );
     }
   }
 
-  getSearchRequest = query => {
-    this.setState({ query });
+  getSearchRequest = (query, images) => {
+    this.setState({ query, page: 1, images: [] });
   };
 
   onNextFetch = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
+    this.setState(prevState => ({ page: prevState.page + 1 }));
   };
-
   toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
-
   openModal = e => {
     const currentImageUrl = e.target.dataset.large;
     const currentImageDescription = e.target.alt;
-
     if (e.target.nodeName === 'IMG') {
       this.setState(({ showModal }) => ({
         showModal: !showModal,
@@ -122,35 +84,25 @@ export class App extends Component {
       }));
     }
   };
-
   render() {
     const {
       images,
-      imagesOnPage,
-      totalImage,
+      showBtn,
       isLoading,
       showModal,
       currentImageUrl,
       currentImageDescription,
     } = this.state;
-
     const getSearchRequest = this.getSearchRequest;
     const onNextFetch = this.onNextFetch;
     const openModal = this.openModal;
     const toggleModal = this.toggleModal;
-
     return (
       <>
         <Searchbar onSubmit={getSearchRequest} />
-
         {images && <ImageGallery images={images} openModal={openModal} />}
-
         {isLoading && <Loader />}
-
-        {imagesOnPage >= 12 && imagesOnPage < totalImage && (
-          <Button onNextFetch={onNextFetch} />
-        )}
-
+        {showBtn && <Button onNextFetch={onNextFetch} />}
         {showModal && (
           <Modal
             onClose={toggleModal}
@@ -158,7 +110,6 @@ export class App extends Component {
             currentImageDescription={currentImageDescription}
           />
         )}
-
         <ToastContainer theme="colored" autoClose={2500} />
       </>
     );
