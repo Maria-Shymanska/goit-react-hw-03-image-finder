39a4ed1ec81e30fef8f24d8cb6c18a2838a74b56
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-17 19:57:19 +0200

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,19 +1,5 @@
-// import axios from 'axios';
-
-// const URL = 'https://pixabay.com/api';
-// const API_KEY = '34465474-c3837bc3938f4efd53294c219';
-
-// function fetchPicture(query, page) {
-//   const response = axios.get(
-//     `?q=${URL}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//   );
-
-//   return response;
-// }
-
-// export default fetchPicture;
 const URL = 'https://pixabay.com/api/';
-const KEY = '34679822-1c5a5d4931a74610a4cbe01cd';
+const KEY = '34465474-c3837bc3938f4efd53294c219';
 const FILTER = '&image_type=photo&orientation=horizontal&per_page=12';
 
 function fetchImages(query, page = 1) {
